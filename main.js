cdb886a9f8c4d0b980cef6064c22b1a691f8a548
Dharmendra Singh <dharmenn@thoughtworks.com>
2015-11-16 13:34:20 +0530

    readline is implemented to display the numbers to user

--- main.js
+++ main.js
@@ -1,34 +1,51 @@
 var magic = {};
+var readline = require('readline');
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
 
-var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
 
 magic.divide_in_obj = function(arr){
 	NumberSets = {};
 	NumberSets["set_1"] = arr.slice(0,7);
 	NumberSets["set_2"] = arr.slice(7,14);
 	NumberSets["set_3"] = arr.slice(14,21);
-	// return Object.keys(NumberSets).map(function(key){
-	// 	return NumberSets[key];
-	// });
 	return NumberSets;
 };
 
-console.log(magic.divide_in_obj(arr));
-
-
-
-magic.userInputHandler = function(input){
-	var divider = magic.divide_in_obj;
+magic.userInputHandler = function(input,setOfNumbers){
+	console.log(input)
 	if(input == 1){
 		return divider([divider(arr).set_2,divider(arr).set_1,divider(arr).set_3]);
-		arr = [divider(arr).set_2,divider(arr).set_1,divider(arr).set_3];
 	}
 	else if(input == 2){
-		arr = [divider(arr).set_1,divider(arr).set_2,divider(arr).set_3];
 		return divider([divider(arr).set_1,divider(arr).set_2,divider(arr).set_3]);
 	}
 	else if(input == 3){
-		arr = [divider(arr).set_1,divider(arr).set_3,divider(arr).set_2];
 		return divider([divider(arr).set_2,divider(arr).set_1,divider(arr).set_3]);
 	}
 };
+
+magic.main = function (){
+	var numbers = magic.arrayOfNumbersGenerator();
+	var setOfNumbers = magic.divide_in_obj(numbers);
+	magic.displayer(setOfNumbers)
+
+};
+
+magic.displayer = function(setOfNumbers){
+	var numbersToDisplay = "SET 1==>  "+setOfNumbers.set_1.join(' ')+"\nSET 2==>  "+setOfNumbers.set_2.join(' ')+'\nSET 3==>  '+setOfNumbers.set_3.join(' ')+'\n';
+	rl.question(numbersToDisplay,magic.userInputHandler);
+}
+
+magic.arrayOfNumbersGenerator = function(){
+	var numbers = [];
+	while(numbers.length<=21){
+		var number = Math.round(Math.random()*21);
+		if(numbers.indexOf(number)==-1) numbers.push(number);
+	};
+	return numbers;
+};
+
+magic.main()
