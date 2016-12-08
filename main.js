3679ce8dea0ed6c94ad5eaa64a0705603b303b30
Akshay <kumarsharmaa69@gmail.com>
2016-12-08 12:15:33 +0530

    moved code to ecmaS 6

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-var magic = {};
 var readline = require('readline');
 var rl = readline.createInterface({
   input: process.stdin,
@@ -6,77 +5,97 @@ var rl = readline.createInterface({
 });
 
 
-magic.divideInBatches = function (numbers) {
-  var batches = {batch_1: [], batch_2: [], batch_3: []};
-  var batchIndex = 0;
-  var keys = Object.keys(batches);
-
-  for (var i = 0; i < numbers.length; i++) {
-    var number = numbers[i];
-    batches[keys[batchIndex]].push(number);
-    batchIndex = (batchIndex + 1) % 3;
+class Trick {
+  constructor(totalLength) {
+    this.totalLength = totalLength || 21;
+    this.timesDisplayed = 0;
   }
 
-  return batches;
-};
+  divideInBatches(numbers) {
+    var batches = {batch_1: [], batch_2: [], batch_3: []};
+    var batchIndex = 0;
+    var keys = Object.keys(batches);
 
-magic.counter = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      var number = numbers[i];
+      batches[keys[batchIndex]].push(number);
+      batchIndex = (batchIndex + 1) % 3;
+    }
 
-magic.showTheNumber = function (numbers) {
-  console.log('Your number: ', numbers[10]);
-};
+    return batches;
+  };
 
-magic.userInputHandler = function (setOfNumbers, magic, input) {
-  var numbers = [];
-  if (input == 1) {
-    numbers = setOfNumbers.batch_2.concat(setOfNumbers.batch_1).concat(setOfNumbers.batch_3);
+  revealNumber(numbers) {
+    return numbers[Math.floor(this.totalLength / 2)];
+  };
+
+  processInput(batches, self, input) {
+    var numbers = [];
+
+    switch (input) {
+      case '1':
+        numbers = batches.batch_2.concat(batches.batch_1).concat(batches.batch_3);
+        break;
+      case "2":
+        numbers = batches.batch_1.concat(batches.batch_2).concat(batches.batch_3);
+        break;
+      case "3":
+        numbers = batches.batch_1.concat(batches.batch_3).concat(batches.batch_2);
+        break;
+      default:
+        throw new Error(input + " is a wrong input");
+    }
+
+    if (self.isTimeToReveal()) {
+      console.log('Your number: ', self.revealNumber(numbers));
+      rl.close();
+    } else {
+      self.show(numbers);
+    }
+  };
+
+  isTimeToReveal() {
+    return this.timesDisplayed == 3;
   }
-  else if (input == 2) {
-    numbers = setOfNumbers.batch_1.concat(setOfNumbers.batch_2).concat(setOfNumbers.batch_3);
+
+  show(numbers) {
+    var setOfNumbers = this.divideInBatches(numbers);
+    console.log(this.timesDisplayed);
+    this.timesDisplayed++;
+    this.display(setOfNumbers)
   }
-  else if (input == 3) {
-    numbers = setOfNumbers.batch_1.concat(setOfNumbers.batch_3).concat(setOfNumbers.batch_2);
-  }
 
-  if (magic.counter == 3) {
-    magic.showTheNumber(numbers);
-    rl.close();
-  } else {
-    magic.main(numbers);
-  }
-};
+  display(batches) {
+    var numbersToDisplay =
+      'Batch 1==>  ' + this.decorate(batches.batch_1) +
+      '\nBatch 2==>  ' + this.decorate(batches.batch_2) +
+      '\nBatch 3==>  ' + this.decorate(batches.batch_3) +
+      '\n';
 
-magic.main = function (numbers) {
-  var setOfNumbers = magic.divideInBatches(numbers);
-  magic.counter++;
-  magic.display(setOfNumbers)
+    rl.question(numbersToDisplay, this.processInput.bind(null, batches, this));
+  };
 
-};
+  numbersGenerator() {
+    var numbers = [];
+    while (numbers.length < 21) {
+      var number = Math.round(Math.random() * 21);
+      if (numbers.indexOf(number) == -1 && number != 0) numbers.push(number);
+    }
+    return numbers;
+  };
 
-magic.display = function (setOfNumbers) {
-  var numbersToDisplay = "SET 1==>  " + magic.decorate(setOfNumbers.batch_1) + "\nSET 2==>  " + magic.decorate(setOfNumbers.batch_2) + '\nSET 3==>  ' + magic.decorate(setOfNumbers.batch_3) + '\n';
-  // console.log(numbersToDisplay);
-  rl.question(numbersToDisplay, magic.userInputHandler.bind(null, setOfNumbers, this));
-};
+  decorate(numbers) {
+    var maxSpaceLength = 6;
+    return numbers.map(function (number) {
+      var spaceLength = maxSpaceLength - String(number).length;
+      var rightPadding = Math.floor(spaceLength / 2);
+      var leftPadding = spaceLength - rightPadding;
 
-magic.numbersGenerator = function () {
-  var numbers = [];
-  while (numbers.length < 21) {
-    var number = Math.round(Math.random() * 21);
-    if (numbers.indexOf(number) == -1 && number != 0) numbers.push(number);
-  }
-  return numbers;
-};
+      return " ".repeat(leftPadding) + number + " ".repeat(rightPadding);
+    }).join("|");
+  };
 
-magic.decorate = function (numbers) {
-  var maxSpaceLength = 6;
-  return numbers.map(function (number) {
-    var spaceLength = maxSpaceLength - String(number).length;
-    var rightPadding = Math.floor(spaceLength / 2);
-    var leftPadding = spaceLength - rightPadding;
+}
 
-    return " ".repeat(leftPadding) + number + " ".repeat(rightPadding);
-  }).join("|");
-};
-
-magic.main(magic.numbersGenerator());
+var trick = new Trick();
+trick.show(trick.numbersGenerator());
