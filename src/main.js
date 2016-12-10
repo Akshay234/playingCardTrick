f2fe4445ec062603ade3d653ef2dfa19f42b1bef
Akshay <kumarsharmaa69@gmail.com>
2016-12-10 22:38:47 +0530

    Added main for executing the trick

---
+++ src/main.js
@@ -0,0 +1,8 @@
+var Trick = require('./Trick21.js');
+
+var trick = new Trick();
+var discription = '==========================================================================================\n'+
+                  'Select any one number from any batch given below and tell which batch consist your number\n'+
+                  '==========================================================================================\n'
+console.log(discription);
+trick.start();
